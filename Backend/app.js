251e41d55331a7852b051c0e986c17fa6d94947e
Abhishek <abhiseknaulae@gmail.com>
2023-02-22 13:02:27 +0530

    porduct route completed ,next error handling

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+
 //Route Imports
 
 const product=require("./routes/productRoute")
--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,8 +1,9 @@
+
 const Product=require('../models/productModel');
 
 
 
-//Create Product 
+//Create Product --> Admin
 
 exports.createProduct=async (req,res)=>{
     console.log("got request",req.body)
@@ -13,6 +14,62 @@ exports.createProduct=async (req,res)=>{
     })
 }
 
-exports.getAllProducts=(req,res)=>{
-    res.status(200).json({message:"Route is working fine"})
+//Get all product 
+exports.getAllProducts=async (req,res)=>{
+    const products= await Product.find();
+    res.status(200).json({
+        products
+    })
+}
+
+//Update Product--- Admin
+
+exports.updateProduct =async (req,res,next)=>{
+    let product = await Product.findById(req.params.id)
+    if(!product)
+    {
+        return res.status(500).json({success:false,
+        message:"Product not found"})
+    }
+    product =await Product.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true,useFindAndModify:false})
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+//Delete product
+
+exports.deleteProduct=async (req,res,next)=>{
+    const product =await Product.findById(req.params.id);
+     
+    if(!product)
+    {
+        return res.status(500).json({
+            success:false,
+            message:"Product not found "
+        })
+    }
+     await product.remove()
+     res.status(200).json({
+        success:true,
+        message:"Product deleted succes fully "
+     })
+}
+
+//get product detials
+
+exports.getProductDetails=async (req,res,next)=>{
+  const product =await Product.findById(req.params.id);
+
+  if(!product)
+  {
+    return res.status(500).json({
+        success:false,
+        message:"Product not found"
+    })
+  }
+  res.status(200).json({
+    success:true,
+    product
+  })
 }
